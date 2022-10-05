cca2cde602901505ba222bf5c2ba11cec7d06dbb
DiogoAntunesOliveira <diogojose11@live.com>
2022-10-05 16:07:38 +0100

    Signatures & Verifiy good and bad signatures

---
+++ Wallet/Models/index.js
@@ -0,0 +1,22 @@
+const { STARTING_BALANCE,} = require('../../config');
+const cryptoHash = require('../../Blockchain/Cryptography/crypto-hash');
+const { ec} = require('../../Blockchain/Cryptography/index');
+
+
+class Wallet{
+    constructor() {
+        this.balance = STARTING_BALANCE;
+
+        // PUBLIC KEY & PRIVATE KEY PAIR
+        this.keyPar = ec.genKeyPair();
+
+        this.publicKey = this.keyPar.getPublic().encode('hex');
+    }
+
+    sign(data){
+        // ec.keyFromPrivate(this.publicKey).sign(data);
+        return  this.keyPar.sign(cryptoHash(data));
+    } 
+} 
+
+module.exports = Wallet;
---
+++ Wallet/Tests/index.test.js
@@ -0,0 +1,48 @@
+const Wallet = require("../Models/index");
+const {verifySignature} = require("../../Blockchain/Cryptography/index");
+
+describe('Wallet', () => {
+    let wallet;
+    
+    beforeEach(() => {
+        wallet = new Wallet();
+    });
+
+    it('has a `balance`', () => {
+        expect(wallet).toHaveProperty('balance');
+    });
+
+    it('has a `publicKey`', () => {
+        console.log(wallet.publicKey);
+        expect(wallet).toHaveProperty('publicKey');
+    });
+
+    describe('signing data', () => {
+        const data = 'foobar';
+
+        it('verifies a signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(true);
+        });
+
+        // User who have created elliptic
+        // indutny/elliptic
+
+        it('does not verify an invalid signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                })
+            ).toBe(false);
+        });  
+    });
+
+
+})
